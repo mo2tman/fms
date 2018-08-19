4900cb01fadf56fdc788f9615e82db2bb732f8f3
Yasser Sadig Ali <yasseralbalola@gmail.com>
2018-08-19 16:04:18 +0200

    uncomment models.py

--- models/models.py
+++ models/models.py
@@ -2,14 +2,14 @@
 
 from odoo import models, fields, api
 
-# class fms(models.Model):
-#     _name = 'fms.fms'
+class fms(models.Model):
+    _name = 'fms.fms'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char()
+    value = fields.Integer()
+    value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+
+    @api.depends('value')
+    def _value_pc(self):
+        self.value2 = float(self.value) / 100
